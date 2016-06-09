76c114a71c23c3fd4ff9f699126d89f605870a77
Alan Gutierrez <alan@prettyrobots.com>
2016-06-09 08:50:05 -0400

    Strip comments no that things have settled.

--- compassion.colleague/confer.js
+++ compassion.colleague/confer.js
@@ -111,25 +111,6 @@ Conference.prototype._message = cadence(function (async, timeout, message) {
             return
         }
 
-        // TODO For now I don't care about the order in which they where
-        // naturalized, ah, yes, I do, because I wanted to use that to generate
-        // a very unique id, even more unique that usual, maybe twice as unique.
-
-        // Create a list of citizens, but with a unique key that combines the
-        // colleague id the promise with which the colleague immigrated.
-
-
-        // Alternate unique id is the identifer plus the promise of the
-        // government that introduced it and then clocks don't matter.
-
-        // Diff exiles from participants.
-        /*
-        this._participants.filter(function (participant) {
-            return citizens.indexOf(participant) == -1
-        }).forEach(function (exile) {
-        }, this)
-        */
-
         // TODO Exile needs to be an object that has the final state. Hmm... No,
         // I believe I left the citizen in there.
         var exile = value.government.exile
@@ -158,31 +139,6 @@ Conference.prototype._message = cadence(function (async, timeout, message) {
 // TODO Put id in this object.
             this._immigrants.push(this._participantIds[immigration.id])
         }
-
-        // Some notes that belong somewhere else.
-
-        // The split between naturalization and immigration doesn't mean we wait
-        // for a participant to naturalize before it can be used within the
-        // application, only that it must naturalize before it can become a
-        // member of the government. This eliminates any concerns that our
-        // leader would assume leadership without the application state. This
-        // addition of eventual naturalization bought a lot and simplified
-        // reasonsing about initalization.
-
-        // Can even sort out back channel synchronization, but we need to still
-        // take advantage of the ability to replay. Leader connects directly to
-        // immigrant, marks the version of the database in the log, streams
-        // state then tells the log to resume for the participant. There can be
-        // a maximum log length held by participant. Pause is a message that
-        // causes the participants kibitzer to pause the log, scanning it for
-        // when to resume the log.
-
-        // We could track the exiles and then remove the tracking when they
-        // become real according to Misnomer. But, this doesn't work because
-        // that history does not exist.
-
-        // Which introduces a race condition where we may recover from a near
-        // fatal collapse, but
 
         if (message.namespace != 'bigeasy.compassion.confer') {
             return
