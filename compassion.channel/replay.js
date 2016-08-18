ccef1c9ff6b2a9f3f1241233f44c87578b04c1b1
Alan Gutierrez <alan@prettyrobots.com>
2016-08-18 08:18:43 -0500

    Upgrade Arguable, complete Channel.
    Closes #59.

--- compassion.channel/replay.js
+++ compassion.channel/replay.js
@@ -1,14 +1,15 @@
-        if (program.ultimate.replay) {
-            var fs = require('fs')
-            var byline = require('byline')
-            var player = colleague
-            colleague.replay()
-            var stream = fs.createReadStream(program.ultimate.replay)
-            byline(stream).on('data', function (line) {
-                if (/^{"/.test(line.toString())) {
-                    colleague.play(JSON.parse(line.toString()))
-                }
-            })
-            stream.on('end', function () { program.emit('SIGINT') })
-        } else {
-        }
+var byline = require('byline')
+var cadence = require('cadence')
+var delta = require('delta')
+
+module.exports = cadence(function (async, stream, colleague) {
+    colleague.replay()
+    delta(async())
+        .ee(byline(stream))
+        .on('data', function (line) {
+            if (/^{"/.test(line.toString())) {
+                colleague.play(JSON.parse(line.toString()))
+            }
+        })
+        .on('end')
+})
