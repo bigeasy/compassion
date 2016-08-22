c029f5da8b4532e0e1698531f5f4ab28c4c4512f
Alan Gutierrez <alan@prettyrobots.com>
2016-08-21 21:03:06 -0500

    Log end of channel.

--- compassion.channel/replay.js
+++ compassion.channel/replay.js
@@ -3,13 +3,17 @@ var cadence = require('cadence')
 var delta = require('delta')
 
 module.exports = cadence(function (async, stream, colleague) {
-    colleague.replay()
-    delta(async())
-        .ee(byline(stream))
-        .on('data', function (line) {
-            if (/^{"/.test(line.toString())) {
-                colleague.play(JSON.parse(line.toString()))
-            }
-        })
-        .on('end')
+    async(function () {
+        colleague.replay()
+        delta(async())
+            .ee(byline(stream))
+            .on('data', function (line) {
+                if (/^{"/.test(line.toString())) {
+                    colleague.play(JSON.parse(line.toString()))
+                }
+            })
+            .on('end')
+    }, function () {
+        console.log('ENDED')
+    })
 })
