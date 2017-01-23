83f22fc4c106e908960c3c323f1601d52691a21f
Alan Gutierrez <alan@prettyrobots.com>
2017-01-23 07:55:36 -0600

    Use the raw socket upgrade.

--- compassion.conduit/conduit.bin.js
+++ compassion.conduit/conduit.bin.js
@@ -38,19 +38,32 @@ require('arguable')(module, require('cadence')(function (async, program) {
 
     var shuttle = Shuttle.shuttle(program, logger)
 
+    var Rendezvous = require('conduit/rendezvous')
+
+    var Upgrader = require('conduit/upgrade')
+    var upgrader = new Upgrader
+
+    var rendezvous = new Rendezvous
+    upgrader.on('socket', rendezvous.upgrade.bind(rendezvous))
+
+    var connect = require('connect')
+    var app = require('connect')()
+        .use(rendezvous.middleware.bind(rendezvous))
+
+    var server = http.createServer(app)
+    server.on('upgrade', upgrader.upgrade.bind(upgrader))
+    destroyer(server)
+
     program.helpIf(program.ultimate.help)
-    program.required('bind', 'id')
+    program.required('bind')
 
     program.validate(require('arguable/bindable'), 'bind')
 
     var bind = program.ultimate.bind
 
-    var ws = require('ws')
-    var conduit = new Conduit(program.ultimate.id)
-    var server = http.createServer(conduit.dispatcher.createWrappedDispatcher())
-    new ws.Server({ server: server }).on('connection', conduit.connection.bind(conduit))
-    destroyer(server)
     server.listen(bind.port, bind.address, async())
     program.on('shutdown', server.destroy.bind(server))
     program.on('shutdown', shuttle.close.bind(shuttle))
+
+    logger.info('started', { bind: bind })
 }))
