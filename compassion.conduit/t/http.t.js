09586c7562d6a09c3529fbac0fd3ae5f45b722ef
Alan Gutierrez <alan@prettyrobots.com>
2017-02-08 19:56:33 -0600

    Unit test for middleware.

---
+++ compassion.conduit/t/http.t.js
@@ -0,0 +1,18 @@
+require('proof')(2, require('cadence')(prove))
+
+function prove (async, assert) {
+    var Conduit = require('../http')
+    var conduit = new Conduit({ _paths: [ '/island/1' ] })
+
+    async(function () {
+        conduit.index(async())
+    }, function (index) {
+        assert(index, 'Compassion Conduit API\n', 'index')
+        conduit.health(async())
+    }, function (health) {
+        assert(health, {
+            health: { occupied: 0, waiting: 0, rejecting: 0, turnstiles: 24 },
+            paths: [ '/island/1' ]
+        }, 'health')
+    })
+}
