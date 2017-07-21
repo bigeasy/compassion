8d3a9180086e7eed5c6059c3a841ab6678366586
Alan Gutierrez <alan@prettyrobots.com>
2017-07-20 22:17:08 -0500

    Remove unused function.

--- compassion.counterfeiter/counterfeiter.generic.js
+++ compassion.counterfeiter/counterfeiter.generic.js
@@ -82,23 +82,6 @@ module.exports = function (Colleague, Conduit) {
         })
     })
 
-    Counterfeiter.prototype._join = cadence(function (async, options) {
-        interrupt.assert(!this.destroyed, 'destroyed', this._destructible.errors[0])
-        // TODO Fold UserAgent into Counterfeiter.
-        var denizen = this._denizens[options.id] = new Denizen(this, options, new UserAgent(this))
-        this.kibitzers[options.id] = denizen.kibitzer
-
-        this._destructible.addDestructor([ 'denizen', options.id ], denizen, 'destroy')
-        this.loggers[options.id] = denizen.logger.shifter()
-        console.log('JOIN')
-        denizen.join(this._port, this._address, options.leader, options.republic, this._destructible.rescue([ 'denizen', options.id ], denizen.ready, 'unlatch'))
-        async(function () {
-            denizen.ready.wait(async())
-        }, function () {
-            this.events[options.id] = denizen.shifter
-        })
-    })
-
     Counterfeiter.prototype.leave = function (id) {
         interrupt.assert(!this.destroyed, 'destroyed', {}, this._destructible.errors[0])
         this._destructible.invokeDestructor([ 'colleague', id ])
