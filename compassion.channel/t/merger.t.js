9312d57616c64b282e4027e3250a12b7f66d580d
Alan Gutierrez <alan@prettyrobots.com>
2018-01-16 08:54:09 -0600

    Test Merger read of empty stream.

--- compassion.channel/t/merger.t.js
+++ compassion.channel/t/merger.t.js
@@ -2,5 +2,23 @@ require('proof')(1, require('cadence')(prove))
 
 function prove (async, okay) {
     var Merger = require('../merger')
-    okay(Merger, 'require')
+    var Procession = require('procession')
+    var cadence = require('cadence')
+    var merger = new Merger({
+        paxos: {
+            outbox: new Procession(),
+            log: new Procession()
+        },
+        islander: { outbox: new Procession() }
+    }, {
+        chatter: new Procession(),
+        getProperties: cadence(function () {
+            throw new Error('conduit#endOfStream')
+        })
+    })
+    async(function () {
+        merger.merge(async())
+    }, function () {
+        okay(true, 'empty stream')
+    })
 }
