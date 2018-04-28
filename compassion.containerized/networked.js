d1c73de27e50631b8e53a82254c095a4b9ca263c
Alan Gutierrez <alan@prettyrobots.com>
2018-04-27 22:14:19 -0500

    Remove heath endpoint for a bit.

--- compassion.containerized/networked.js
+++ compassion.containerized/networked.js
@@ -17,7 +17,6 @@ function Networked (destructible, colleagues) {
         dispatcher.dispatch('POST /island/:island/islander/:id/kibitz', 'kibitz')
         dispatcher.dispatch('POST /island/:island/islander/:id/broadcasts', 'broadcasts')
         dispatcher.dispatch('POST /island/:island/islander/:id/backlog', 'backlog')
-        dispatcher.dispatch('GET /health', 'health')
     })
 }
 
@@ -62,21 +61,6 @@ Networked.prototype.backlog = cadence(function (async, request, island, id) {
     }, function (stream, response) {
         return [ 200, response.headers, function (response) { stream.pipe(response) } ]
     })
-})
-
-Networked.prototype.health = cadence(function (async) {
-    var colleagues = []
-    for (var island in this._islands) {
-        for (var id in this._islands[island]) {
-            var colleague = this._islands[island][id]
-            colleagues.push({
-                initalizer: colleagues.initalizer,
-                government: colleagues.kibitzer.paxos.government,
-                initialized: false
-            })
-        }
-    }
-    return { colleagues: colleagues }
 })
 
 module.exports = Networked
