cd5e923f11b8099407a3e0a2dd005a81ee5a778b
Alan Gutierrez <alan@prettyrobots.com>
2018-04-28 11:28:18 -0500

    Sketch of selecting an embark point.

---
+++ compassion.containerized/embark.js
@@ -0,0 +1,35 @@
+var Monotonic = require('monotonic').asString
+
+module.exports = function (members, republic) {
+    var unsplit = members.filter(function (member) {
+        return member.government.promise != '0/0' && member.government.republic == republic
+    })
+
+    // The entire island has disappeared.
+    if (unsplit.length == 0) {
+        return { action: 'terminate' }
+    }
+
+    unsplit.sort(function (a, b) {
+        return -Monotonic.compare(a.government.promise, b.government.promise)
+    })
+
+    var government = unsplit[0].government
+
+    var ids = unsplit.map(function (member) { return member.id })
+
+    // Cannot find a quorum, unrecoverable.
+    if (government.majority.concat(government.minority).filter(function (id) {
+        return ~ids.indexOf(id)
+    }).length < government.majority.length) {
+        return { action: 'terminate' }
+    }
+
+    if (government.majority.filter(function (id) {
+        return ~ids.indexOf(id)
+    }).length == government.majority.length) {
+        return { action: 'embark', url: government.properties[government.majority[0]].url }
+    }
+
+    return { action: 'retry' }
+}
---
+++ compassion.containerized/t/embark.t.js
@@ -0,0 +1,85 @@
+require('proof')(4, prove)
+
+function prove (okay) {
+    var embark = require('../embark')
+    okay(embark([{
+        id: 'x',
+        government: {
+            promise: '0/0',
+            republic: 1
+        }
+    }], 1), {
+        action: 'terminate'
+    }, 'no members')
+    okay(embark([{
+        id: 'x',
+        government: {
+            promise: '0/0',
+            republic: 1
+        }
+    }, {
+        id: 'a',
+        government: {
+            republic: 1,
+            promise: '6/0',
+            majority: [ 'a', 'b' ],
+            minority: [ 'c' ]
+        }
+    }], 1), {
+        action: 'terminate'
+    }, 'collapsed')
+    okay(embark([{
+        id: 'x',
+        government: {
+            promise: '0/0',
+            republic: 1
+        }
+    }, {
+        id: 'a',
+        government: {
+            republic: 1,
+            promise: '6/0',
+            majority: [ 'a', 'b' ],
+            minority: [ 'c' ]
+        }
+    }, {
+        id: 'c',
+        government: {
+            republic: 1,
+            promise: '6/0',
+            majority: [ 'a', 'b' ],
+            minority: [ 'c' ]
+        }
+    }], 1), {
+        action: 'retry'
+    }, 'retry')
+    okay(embark([{
+        id: 'x',
+        government: {
+            promise: '0/0',
+            republic: 1
+        }
+    }, {
+        id: 'a',
+        government: {
+            republic: 1,
+            promise: '7/1',
+            majority: [ 'a', 'b' ],
+            minority: [ 'c' ],
+            properties: {
+                a: { url: 'http://127.0.0.1:8080/' }
+            }
+        }
+    }, {
+        id: 'b',
+        government: {
+            republic: 1,
+            promise: '7/0',
+            majority: [ 'a', 'b' ],
+            minority: [ 'c' ]
+        }
+    }], 1), {
+        action: 'embark',
+        url: 'http://127.0.0.1:8080/'
+    }, 'embark')
+}
