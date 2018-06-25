517485f049359478b8d4b277b8a7cfdd6a48aa33
Alan Gutierrez <alan@prettyrobots.com>
2018-06-25 16:02:12 -0400

    Create a Compassion executable.

---
+++ compassion.colleague/t/colleague.bin.t.js
@@ -0,0 +1,50 @@
+require('proof')(2, require('cadence')(prove))
+
+function prove (async, okay) {
+    var bin = require('..'), io
+    var Mock = require('olio/mock')
+    var Procedure = require('conduit/procedure')
+    var Destructible = require('destructible')
+    var destructible = new Destructible('t/mock')
+    var UserAgent = require('vizsla')
+    var ua = new UserAgent
+    async([function  () {
+        destructible.completed.wait(async())
+    }, function (error) {
+        console.log(error.stack)
+        throw error
+    }])
+    async([function () {
+        destructible.destroy()
+    }], function () {
+        io = bin([ '--local', 8386, '--network', 8486, '--discovery', 'mingle/static' ], {}, async())
+        var mock = new Mock(io)
+        async(function () {
+            mock.ready.wait(async())
+        }, function () {
+            mock.initialize([ 'program', 'self' ], 0)
+            mock.sibling([ 'mingle', 'static' ], 1, function (destructible, index, count, callback) {
+                destructible.monitor('procedure', Procedure, function () {}, callback)
+            })
+        })
+        async(function () {
+            io.ready.wait(async())
+        }, function () {
+            ua.fetch({
+                url: 'http://127.0.0.1:8486/',
+                parse: 'text',
+                timeout: 1000
+            }, async())
+        }, function (body) {
+            okay(body, 'Compassion Networked API\n', 'network interface')
+            ua.fetch({
+                url: 'http://127.0.0.1:8386/',
+                parse: 'text',
+                timeout: 1000
+            }, async())
+        }, function (body) {
+            okay(body, 'Compassion Local API\n', 'local interface')
+            io.emit('SIGTERM')
+        })
+    })
+}
