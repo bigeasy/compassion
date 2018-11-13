1ec327f4e8db3a5651f74d520386c4eed241eee6
Alan Gutierrez <alan@prettyrobots.com>
2018-11-13 00:28:00 -0600

    Tidy unit test.

--- compassion.counterfeiter/t/counterfeiter.t.js
+++ compassion.counterfeiter/t/counterfeiter.t.js
@@ -1,14 +1,19 @@
-require('proof')(1, require('cadence')(prove))
+require('proof')(1, prove)
 
-function prove (async, okay) {
+function prove (okay, callback) {
     var Counterfeiter = require('..')
     var Destructible = require('destructible')
     var destructible = new Destructible('t/counterfeiter.t.js')
-    async([function () {
-        destructible.destroy()
-    }], function () {
-        destructible.monitor('counterfeiter', Counterfeiter, {}, async())
-    }, function (counterfeiter) {
-        okay(counterfeiter, 'created')
-    })
+
+    destructible.completed.wait(callback)
+
+    var cadence = require('cadence')
+
+    cadence(function (async) {
+        async(function () {
+            destructible.monitor('counterfeiter', Counterfeiter, {}, async())
+        }, function (counterfeiter) {
+            okay(counterfeiter, 'created')
+        })
+    })(destructible.monitor('test'))
 }
