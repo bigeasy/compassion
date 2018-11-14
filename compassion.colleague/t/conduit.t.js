404123b41de4087e284f68251ad7d053514895c4
Alan Gutierrez <alan@prettyrobots.com>
2018-11-13 19:49:52 -0600

    Rename `procedure` to `conduit`.

--- compassion.colleague/t/conduit.t.js
+++ compassion.colleague/t/conduit.t.js
@@ -2,9 +2,9 @@ require('proof')(1, require('cadence')(prove))
 
 function prove (async, okay) {
     var Procession = require('procession')
-    var Procedure = require('../resolver/procedure')
+    var Resolver = require('../resolver/conduit')
     async(function () {
-        new Procedure({
+        new Resolver({
             connect: function (envelope, callback) {
                 var inbox = new Procession
                 var shifter = inbox.shifter()
