af331376384f547168bee2140a408b7a7fe4f741
Alan Gutierrez <alan@prettyrobots.com>
2019-08-10 23:10:53 -0400

    Convert `var` to `const`.

--- compassion.colleague/recoverable.js
+++ compassion.colleague/recoverable.js
@@ -1,4 +1,4 @@
-var Monotonic  = require('monotonic').asString
+const Monotonic  = require('monotonic').asString
 
 module.exports =  function (id, members) {
     // Never mind.
@@ -7,13 +7,13 @@ module.exports =  function (id, members) {
     }
 
     // Find ourselves.
-    var self = members.filter(function (member) {
+    const selves = members.filter(function (member) {
         return member.id == id
     })
 
     // If we can't even reach ourselves, then let's hang on. No point in
     // restarting because we're not going to be able to join.
-    if (self.length != 1) {
+    if (selves.length != 1) {
         return { action: 'retry' }
     }
 
@@ -21,11 +21,11 @@ module.exports =  function (id, members) {
         return b.government.republic - a.government.republic
     })
 
-    var unsplit = members.filter(function (member) {
+    const unsplit = members.filter(function (member) {
         return member.government.republic == members[0].government.republic
     })
 
-    self = self.shift()
+    const self = selves.shift()
 
     // Our republic has been superseded. This one is definitive.
     if (members[0].government.republic != self.government.republic) {
@@ -36,8 +36,8 @@ module.exports =  function (id, members) {
         return -Monotonic.compare(a.government.promise, b.government.promise)
     })
 
-    var government = unsplit[0].government
-    var ids = unsplit.map(function (member) { return member.id })
+    const government = unsplit[0].government
+    const ids = unsplit.map(function (member) { return member.id })
 
     if (government.majority.concat(government.minority).filter(function (id) {
         return !! ~ids.indexOf(id)
