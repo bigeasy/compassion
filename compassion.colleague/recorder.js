07e3c4a9e768ba0ec28624d69a6aa10c124810a9
Alan Gutierrez <alan@prettyrobots.com>
2019-08-11 18:58:07 -0400

    Convert Recorder to ES6.

--- compassion.colleague/recorder.js
+++ compassion.colleague/recorder.js
@@ -1,12 +1,14 @@
-function Recorder (events, id, type) {
-    this._events = events
-    this._id = id
-    this._type = type
-}
+class Recorder {
+    constructor (events, id, type) {
+        this._events = events
+        this._id = id
+        this._type = type
+    }
 
-Recorder.prototype.record = function (body) {
-    if (body != null) {
-        this._events.push({ type: this._type, id: this._id, body: JSON.parse(JSON.stringify(body)) })
+    record (body) {
+        if (body != null) {
+            this._events.push({ type: this._type, id: this._id, body: JSON.parse(JSON.stringify(body)) })
+        }
     }
 }
 
