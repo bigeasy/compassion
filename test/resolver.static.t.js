8cbd20de03d53fb0311d08aaaaa1572a91d265c5
Alan Gutierrez <alan@prettyrobots.com>
2021-03-10 22:07:20 -0600

    Remove old multi-process code.

--- test/resolver.static.t.js
+++
@@ -1,6 +0,0 @@
-require('proof')(1, (okay) => {
-    const Resolver = { Static: require('../resolver/static') }
-    const urls = [ 'http://127.0.0.1:8080' ]
-    const resolver = new Resolver.Static(urls)
-    okay(resolver.resolve(), urls, 'static')
-})
