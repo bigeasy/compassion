b189e75c5431748ee9300d58515126c93ac05a86
Alan Gutierrez <alan@prettyrobots.com>
2021-10-11 00:51:00 -0500

    Convert `README.md` to Edify.

---
+++ test/readme.t.js
@@ -0,0 +1,34 @@
+// [![Actions Status](https://github.com/bigeasy/compassion/workflows/Node%20CI/badge.svg)](https://github.com/bigeasy/compassion/actions)
+// [![codecov](https://codecov.io/gh/bigeasy/compassion/branch/master/graph/badge.svg)](https://codecov.io/gh/bigeasy/compassion)
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//
+// Paxos based consensus framework.
+//
+// | What          | Where                                             |
+// | --- | --- |
+// | Discussion    | https://github.com/bigeasy/compassion/issues/1    |
+// | Documentation | https://bigeasy.github.io/compassion              |
+// | Source        | https://github.com/bigeasy/compassion             |
+// | Issues        | https://github.com/bigeasy/compassion/issues      |
+// | CI            | https://travis-ci.org/bigeasy/compassion          |
+// | Coverage:     | https://codecov.io/gh/bigeasy/compassion          |
+// | License:      | MIT                                               |
+//
+//
+// Compassion installs from NPM.
+
+// ## Living `README.md`
+//
+// This `README.md` is also a unit test using the
+// [Proof](https://github.com/bigeasy/proof) unit test framework. We'll use the
+// Proof `okay` function to assert out statements in the readme. A Proof unit test
+// generally looks like this.
+
+require('proof')(1, okay => {
+    // ## Overview
+
+    okay('TODO')
+})
+
+// You can run this unit test yourself to see the output from the various
+// code sections of the readme.
